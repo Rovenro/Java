90ef263688c0d5190655e40fce0168fa69886f55
Roven <1311867635@qq.com>
2026-03-26 08:24:16 +0800

    Add files via upload
    死循环

---
+++ wuxianlooper/InfiniteLoopDemo.java
@@ -0,0 +1,22 @@
+package wuxianlooper;
+
+public class InfiniteLoopDemo {
+    static void main(String[] args) {
+        /*
+        无限循环
+        注意无限循环下面不能写任何代码
+         */
+
+         /*for(;;){
+              System.out.println("你好");
+          }*/
+
+        /*while(true){
+            System.out.println("你好");
+        }*/
+
+        do {
+            System.out.println("你好");
+        } while (true);
+    }
+}
